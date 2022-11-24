58001ed53138b441b2fa2bb5bb0cd47b97c20896
chester <ronchester.mendoza@gmail.com>
2022-11-24 16:04:15 +0800

    makes validation into one line

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -12,7 +12,6 @@
 #
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }
 end
