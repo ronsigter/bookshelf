68aa3cf91e2e181a27d2ead1da750f76ad7dc002
chester <ronchester.mendoza@gmail.com>
2022-11-25 23:55:54 +0800

    adds controller to authenticate user

---
+++ backend/app/controllers/authentication_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AuthenticationController < ApplicationController
+  def login
+    user = User.find_by(username: login_params[:username])
+
+    if user&.authenticate(login_params[:password])
+      token = JwtService.encode(user_id: user.id)
+      render(json: { data: { token: token, username: user.username } }, status: :created)
+    else
+      render(json: { errors: ["Unauthorized"] }, status: :unauthorized)
+    end
+  end
+
+  private
+
+  def login_params
+    params.require(:login).permit(:username, :password)
+  end
+end
--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :uuid             not null, primary key
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     username { Faker::Internet.username }
