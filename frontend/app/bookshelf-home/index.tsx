e1c65d70c1038ba26d684c95c96349ccd82b43c6
chester <ronchester.mendoza@gmail.com>
2022-11-26 00:32:51 +0800

    removes unnecessary files

---
+++ frontend/app/bookshelf-home/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import { Home } from './Home'
+
+const IndexContainer: NextPage = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default IndexContainer
