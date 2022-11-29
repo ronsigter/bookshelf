e69d7317e40b618696c18be9489b0bcceab98409
chester <ronchester.mendoza@gmail.com>
2022-11-29 17:08:59 +0800

    creates authorization function

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header
+
+    begin
+      decoded = JwtService.decode(token)
+      @user = User.find(decoded[:user_id])
+    rescue
+      render(json: { errors: ["Access denied due to invalid credentials"] }, status: :unauthorized)
+    end
+  end
 end
