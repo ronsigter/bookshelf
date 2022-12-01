04a1be6c68ecec2273d56b72040629261be3c50d
chester <ronchester.mendoza@gmail.com>
2022-12-01 17:58:11 +0800

    adds logo

--- frontend/app/bookshelf-login/index.tsx
+++ frontend/app/bookshelf-login/index.tsx
@@ -1,11 +1,15 @@
 import { Button } from 'components/buttons/Button'
 import { Input } from 'components/forms/Input'
 import type { NextPage } from 'next'
+import Image from 'next/image'
 
 const LoginContainer: NextPage = () => {
   return (
     <div className="bg-background flex h-screen w-full justify-center">
       <div className="flex w-full max-w-sm flex-col items-center justify-center px-4">
+        <div className="py-5">
+          <Image width={200} height={200} alt="bookney-logo" src="/bookney-light.png" />
+        </div>
         <form className="flex w-full flex-col gap-4">
           <Input label="Username" />
           <Input label="Password" type="password" />
