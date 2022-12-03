6d07c815b3ea268f472687cdf6a539d0d83be156
chester <ronchester.mendoza@gmail.com>
2022-12-03 13:15:25 +0800

    ditches css files

--- frontend/components/buttons/Button.tsx
+++ frontend/components/buttons/Button.tsx
@@ -6,7 +6,10 @@ export const Button: React.FC<ButtonProps> = (props) => {
   const { label, ...rest } = props
 
   return (
-    <button {...rest} className="btn">
+    <button
+      {...rest}
+      className="bg-primary rounded-lg p-2.5 text-white transition duration-200 hover:-translate-y-0.5 hover:bg-[#436fa8] focus:outline-none focus:ring-0"
+    >
       {label}
     </button>
   )
--- frontend/components/forms/Input.tsx
+++ frontend/components/forms/Input.tsx
@@ -6,9 +6,18 @@ export const Input: React.FC<InputProps> = (props) => {
   const { label, ...rest } = props
 
   return (
-    <div className="form-input">
-      <input {...rest} id={label} placeholder=" " className="input" />
-      <label htmlFor={label} className="label">
+    <div className="relative text-white">
+      <input
+        {...rest}
+        id={label}
+        placeholder=" "
+        className="focus:border-primary peer block w-full appearance-none rounded-lg border-2 border-gray-300 bg-transparent px-2.5 pb-2.5  pt-4 text-sm focus:outline-none focus:ring-0"
+      />
+      <label
+        htmlFor={label}
+        className="peer-focus:peer-placeholder-shown:text-primary peer-focus:peer-placeholder-shown:bg-background absolute top-3 left-1 z-10 origin-[0] px-2 py-1 text-sm text-gray-400
+        transition duration-200 peer-focus:peer-placeholder-shown:-translate-y-[1.6rem] peer-focus:peer-placeholder-shown:translate-x-1 peer-focus:peer-placeholder-shown:scale-75"
+      >
         {label}
       </label>
     </div>
