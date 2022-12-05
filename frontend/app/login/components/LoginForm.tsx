a688f855a771e19a680784cf03f48c299857470f
chester <ronchester.mendoza@gmail.com>
2022-12-05 17:01:36 +0800

    calls mutation for login

--- frontend/app/login/components/LoginForm.tsx
+++ frontend/app/login/components/LoginForm.tsx
@@ -1,17 +1,33 @@
-'use client'
-
 import { Button } from 'components/buttons/Button'
 import { Input } from 'components/forms/Input'
+import { useForm } from 'react-hook-form'
+import useLogin from '../hooks/useLogin'
+
+type FormType = {
+  username: string
+  password: string
+}
 
 export const LoginForm: React.FC = () => {
-  const handleOnSubmit = () => {
-    console.log('SUBMITTED!')
+  const { mutate } = useLogin()
+  const { handleSubmit, register } = useForm<FormType>()
+
+  const onSubmit = async (formdata: FormType): Promise<void> => {
+    mutate(formdata, {
+      onSuccess: (data) => {
+        // TODO: Save token to cookies
+        console.log('token', data.token)
+      },
+      // TODO: Show error messages
+      onError: (error) => console.log(error)
+    })
   }
 
   return (
-    <form className="flex w-full flex-col gap-4" onSubmit={handleOnSubmit}>
-      <Input label="Username" />
-      <Input label="Password" type="password" />
+    <form className="flex w-full flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+      <Input label="Username" {...register('username', { required: true })} />
+      <Input label="Password" type="password" {...register('password', { required: true })} />
+      {/* TODO: Add loading indicator */}
       <Button label="Sign In" />
     </form>
   )
