0b0b8bacdf82098938d7c1d87ae03e2dce1c263f
chester <ronchester.mendoza@gmail.com>
2022-12-05 16:59:31 +0800

    adds react-query provider

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,15 +1,22 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Image from 'next/image'
 import { LoginForm } from './components/LoginForm'
 
+const queryClient = new QueryClient()
+
 export default function LoginPage() {
   return (
-    <div className="bg-background flex h-screen w-full justify-center">
-      <div className="flex w-full max-w-sm flex-col items-center justify-center px-4">
-        <div className="py-5">
-          <Image width={200} height={200} alt="bookney-logo" src="/bookney-light.png" />
+    <QueryClientProvider client={queryClient}>
+      <div className="bg-background flex h-screen w-full justify-center">
+        <div className="flex w-full max-w-sm flex-col items-center justify-center px-4">
+          <div className="py-5">
+            <Image width={200} height={200} alt="bookney-logo" src="/bookney-light.png" />
+          </div>
+          <LoginForm />
         </div>
-        <LoginForm />
       </div>
-    </div>
+    </QueryClientProvider>
   )
 }
