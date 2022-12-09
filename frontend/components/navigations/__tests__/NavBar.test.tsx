9b042e317ddfd8a0e2e0e4a201672960e4423967
chester <ronchester.mendoza@gmail.com>
2022-12-10 02:17:27 +0800

    adds navbar test

---
+++ frontend/components/navigations/__tests__/NavBar.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen, userEvent } from 'lib/jest'
+import { NavBar } from '../NavBar'
+
+const mockRouterReplace = jest.fn()
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      replace: mockRouterReplace
+    }
+  }
+}))
+
+const mockRemoveCookie = jest.fn()
+jest.mock('cookies-next', () => ({
+  deleteCookie(key: string) {
+    mockRemoveCookie(key)
+  }
+}))
+
+describe('<Navbar />', () => {
+  const user = userEvent.setup()
+
+  it('renders the NavBar elements', () => {
+    render(<NavBar />)
+
+    const links = screen.getAllByRole('link')
+    expect(links[0]).toHaveAttribute('href', '/dashboard')
+    expect(links[1]).toHaveAttribute('href', '#')
+    expect(links[2]).toHaveAttribute('href', '#')
+    expect(links[3]).toHaveAttribute('href', '#')
+    expect(screen.getByRole('button', { name: /sign out/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /bookney-logo/i })).toBeInTheDocument()
+  })
+
+  it('log out signed user', async () => {
+    render(<NavBar />)
+
+    await user.click(screen.getByRole('button', { name: /sign out/i }))
+
+    expect(mockRemoveCookie).toHaveBeenCalledWith('token')
+    expect(mockRouterReplace).toHaveBeenCalledWith('/login')
+  })
+})
