8d50fb788890400f814a7f2099b04255bd2ca2c8
chester <ronchester.mendoza@gmail.com>
2022-12-10 01:38:29 +0800

    adds route protect to /dashboard page

---
+++ frontend/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('token')?.value
+
+  if (!token) return NextResponse.redirect(new URL('/login', req.url))
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/dashboard']
+}
