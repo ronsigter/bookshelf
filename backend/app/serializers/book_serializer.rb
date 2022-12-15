162409af3baf1ec0696237e8b7c5a7dc92b59ef2
chester <ronchester.mendoza@gmail.com>
2022-12-15 17:57:12 +0800

    adds annotation

--- backend/app/serializers/book_serializer.rb
+++ backend/app/serializers/book_serializer.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: books
+#
+#  id          :uuid             not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_books_on_title  (title)
+#
 class BookSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
