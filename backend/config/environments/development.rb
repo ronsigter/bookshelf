ff28bb6f4f0c6371e130cdd5ea50dd4e97fb5038
chester <ronchester.mendoza@gmail.com>
2022-12-15 14:36:59 +0800

    setups active storage to use cloudinary

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -55,6 +55,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :cloudinary
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
