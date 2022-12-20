0ecc4d8825fc7af4f8e31158cd7fe10738e45f12
chester <ronchester.mendoza@gmail.com>
2022-12-20 17:03:26 +0800

    creates fetch for reading list

---
+++ frontend/services/reading-list.ts
@@ -0,0 +1,78 @@
+import { redirect } from 'next/navigation'
+import type { Session } from 'next-auth'
+import type { ReadingListStatus } from './books'
+
+const REST_SERVER = process.env.REST_SERVER || ''
+
+type AddToReadingList = (session: Session, params: { book_id: string }) => Promise<void>
+
+const headers = (token: string): HeadersInit => ({
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  Authorization: `bearer ${token}`
+})
+
+export const addToReadingList: AddToReadingList = async (session, params) => {
+  const { book_id } = params
+  if (!session) redirect('/login')
+
+  const response = await fetch(`${REST_SERVER}/api/v1/reading_list`, {
+    method: 'POST',
+    headers: headers(session.accessToken),
+    body: JSON.stringify({
+      reading_list: {
+        book_id
+      }
+    })
+  })
+
+  if (!response.ok) throw new Error(response.statusText)
+
+  const data = await response.json()
+
+  return data
+}
+
+type RemoveFromReadingList = (session: Session, params: { book_id: string }) => Promise<void>
+
+export const removeFromReadingList: RemoveFromReadingList = async (session, params) => {
+  const { book_id } = params
+  if (!session) redirect('/login')
+
+  const response = await fetch(`${REST_SERVER}/api/v1/reading_list/${book_id}`, {
+    method: 'DELETE',
+    headers: headers(session.accessToken)
+  })
+
+  if (!response.ok) throw new Error(response.statusText)
+
+  const data = await response.json()
+
+  return data
+}
+
+type UpdateReadingListStatus = (
+  session: Session,
+  params: { book_id: string; status: ReadingListStatus }
+) => Promise<void>
+
+export const updateReadingListStatus: UpdateReadingListStatus = async (session, params) => {
+  const { book_id, status } = params
+  if (!session) redirect('/login')
+
+  const response = await fetch(`${REST_SERVER}/api/v1/reading_list/${book_id}`, {
+    method: 'PUT',
+    headers: headers(session.accessToken),
+    body: JSON.stringify({
+      reading_list: {
+        status
+      }
+    })
+  })
+
+  if (!response.ok) throw new Error(response.statusText)
+
+  const data = await response.json()
+
+  return data
+}
