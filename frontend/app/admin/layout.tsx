56e1cb062c4b41bc52a63a26cb73bd833190442b
chester <ronchester.mendoza@gmail.com>
2022-12-22 02:55:17 +0800

    adds initial UI for creating book reacord

---
+++ frontend/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { NavBar } from 'components/navigations/NavBar'
+import AuthenthicatorWrapper from 'components/AuthenticatorWrapper'
+
+// ? Issue is that TypeScript doesn't understand async components
+// ? adding "/* @ts-expect-error Server Component */" from these recommendations
+// ? https://beta.nextjs.org/docs/configuring/typescript#end-to-end-type-safety
+// ? https://github.com/vercel/next.js/issues/42292
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    /* @ts-expect-error Server Component */
+    <AuthenthicatorWrapper>
+      <div>
+        <NavBar />
+        {children}
+      </div>
+    </AuthenthicatorWrapper>
+  )
+}
