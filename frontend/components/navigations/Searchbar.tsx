6599b38f77294a2c7865c17bae4fe3eff9adabae
chester <ronchester.mendoza@gmail.com>
2023-01-06 22:16:54 +0800

    style searchbar

--- frontend/components/navigations/Searchbar.tsx
+++ frontend/components/navigations/Searchbar.tsx
@@ -21,8 +21,9 @@ export const Searchbar: React.FC = () => {
         <input
           ref={ref}
           id="search"
+          autoComplete="off"
           placeholder="Title, author"
-          className="focus:border-primary transition-width [&:not(:placeholder-shown)]:border-primary peer block w-[0px] appearance-none rounded-lg border-2 border-transparent bg-transparent px-2.5 pl-5  pb-2.5 pt-2.5 text-sm text-white duration-500 focus:w-[280px] focus:pl-9 focus:outline-none focus:ring-0 [&:not(:placeholder-shown)]:w-[280px] [&:not(:placeholder-shown)]:pl-9"
+          className="focus:border-primary transition-width [&:not(:placeholder-shown)]:border-primary peer block w-[0px] appearance-none rounded-lg border-2 border-transparent bg-transparent px-2.5 pl-5  pb-2.5 pt-2.5 text-sm text-white duration-500 focus:w-[280px] focus:pl-9 focus:outline-none focus:ring-0 [&:not(:placeholder-shown)]:w-[280px] [&:not(:placeholder-shown)]:pl-9 [&:not(:focus):not(:placeholder-shown)]:border-white"
         />
       </label>
     </form>
