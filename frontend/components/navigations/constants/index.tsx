b2f3b75f54683ca8db36f9c959e715b82a735bbf
chester <ronchester.mendoza@gmail.com>
2023-01-10 19:25:31 +0800

    removes search tab

--- frontend/components/navigations/constants/index.tsx
+++ frontend/components/navigations/constants/index.tsx
@@ -2,6 +2,5 @@ import { FaHome, FaSearch, FaBook } from 'react-icons/fa'
 
 export const MENUS = [
   { label: 'home', path: '/dashboard', icon: <FaHome size={16} /> },
-  { label: 'search', path: '/search', icon: <FaSearch size={16} /> },
   { label: 'reading list', path: '/my-list', icon: <FaBook size={16} /> }
 ]
