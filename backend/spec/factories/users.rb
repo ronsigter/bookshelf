2793f51772edfe535e8e707984066e3d95b786ea
chester <ronchester.mendoza@gmail.com>
2023-01-17 17:26:41 +0800

    configures test to get all books of user

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -18,5 +18,15 @@ FactoryBot.define do
   factory :user do
     username { Faker::Internet.username }
     password { Faker::Internet.password(min_length: 8) }
+
+    factory :user_with_books do
+      transient do
+        books_count { 10 }
+      end
+
+      books do
+        Array.new(books_count) { association(:book) }
+      end
+    end
   end
 end
